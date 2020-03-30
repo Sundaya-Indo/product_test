0861b774100f84a30b6109ea878014cca810922f
agisna <agis@sundaya.com>
2020-03-30 11:07:25 +0700

    fix baru

---
+++ models/employee_template.py
@@ -0,0 +1,8 @@
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+class Employees(models.Model):
+    _inherit = 'hr.employee'
+
+    # name = fields.Char(string='Label dari Field')

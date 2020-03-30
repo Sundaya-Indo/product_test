eed0d4815e2534b0e4451126ed0bc26c6155749c
agisna <agis@sundaya.com>
2020-03-30 10:33:24 +0700

    Revert "fix employee"
    This reverts commit 2e4377572835aefbd6c397879cc6cd017b135aa6.

--- models/employee_template.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api, _
-from odoo.exceptions import UserError
-
-class Employees(models.Model):
-    _inherit = 'hr.employee'
-
-    # name = fields.Char(string='Label dari Field')

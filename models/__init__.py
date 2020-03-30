fc87fab9c6ad9b7fbfc2bb7732011f6d92ab23dc
agisna <agis@sundaya.com>
2020-03-30 10:17:27 +0700

    fix employee 2

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
 from . import product_templates
 from . import ir_attachment
-from . import contact_template
-=======
-from . import product_templates
-from . import ir_attachment
-<<<<<<< HEAD
-from . import contact_template
 from . import employee_template
->>>>>>> 2e4377572835aefbd6c397879cc6cd017b135aa6
-=======
 from . import contact_template
->>>>>>> parent of 2e43775... fix employee

4cc5b259f78d436e4d67ab3bbd21d6acf84969ce
agisna <agis@sundaya.com>
2020-04-02 16:52:18 +0700

    map 1

--- models/contact_template.py
+++ models/contact_template.py
@@ -4,5 +4,5 @@ from odoo.exceptions import UserError
 class Partners(models.Model):
     _inherit = 'res.partner'
 
-    # is_employee = fields.Boolean(string='Is an Employee', default="false")
+    # map = fields.Char(Str="map")
     

189e53228b62a21bb66e2a4e24dd21347a5f6c18
agisna <agis@sundaya.com>
2020-09-29 08:36:18 +0700

    fix ir attachment

--- models/ir_attachments.py
+++ models/ir_attachments.py
@@ -3,10 +3,10 @@ from odoo.exceptions import UserError
 
 class IrAttachment(models.Model):
     _inherit = 'ir.attachment'
-    
+
     data = fields.Binary(string='data', compute='get_datas')
     
-    @api.depends('datas',)
+    @api.depends('datas')
     def get_datas(self):
         for item in self:
             item.data = item.datas

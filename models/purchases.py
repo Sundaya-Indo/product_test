058c31c19b5d329e4a68cbad9b023ab74b046fd7
agisna <agis@sundaya.com>
2020-10-28 16:44:42 +0700

    add approval sign

--- models/purchases.py
+++ models/purchases.py
@@ -6,5 +6,19 @@ class PurchaseOrder(models.Model):
 
     # delivery_date = fields.Datetime('Delivery Date', default=fields.Datetime.now)
     shipping_mode = fields.Selection([ ('by_air', 'By Air'),('by_sea', 'By Sea'),('by_courier', 'By Courier')],'Shipping Mode', default='by_courier')
-    res_user_id = fields.Many2one('res.users', store=True)
+    res_user_id = fields.Many2one('res.users', compute='sign_po', store=True)
+    current_user = fields.Many2one('res.users', 'Current User')
+
+    @api.multi
+    def sign_po(self):
+        for doc in self:
+            if doc.state == 'draft':
+                temp = self.env.uid
+                doc.res_user_id = temp
+            else:
+                pass
+
+
+
+
     
